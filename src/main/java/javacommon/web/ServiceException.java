46cf02c24b2779a1cfac2d110adccad710670d99
zhengwei <zhengwei223@163.com>
2016-07-18 19:40:31 +0800

    000

--- src/main/java/javacommon/web/ServiceException.java
+++ src/main/java/javacommon/web/ServiceException.java
@@ -1,5 +1,7 @@
 package javacommon.web;
 
+import org.springframework.http.HttpStatus;
+
 import javacommon.web.ErrorCode;
 
 public class ServiceException extends RuntimeException {
@@ -7,9 +9,29 @@ public class ServiceException extends RuntimeException {
 	private static final long serialVersionUID = -8634700792767837033L;
 
 	public ErrorCode errorCode;
+	public HttpStatus status;
+	
+	public ServiceException() {
+		super();
+	}
 
+	public ServiceException(String message) {
+		super(message);
+	}
+
+	public ServiceException(Throwable cause) {
+		super(cause);
+	}
+
+	public ServiceException(String message, Throwable cause) {
+		super(message, cause);
+	}
 	public ServiceException(String message, ErrorCode errorCode) {
 		super(message);
 		this.errorCode = errorCode;
+	}
+	public ServiceException(HttpStatus status, String message) {
+		super(message);
+		this.status = status;
 	}
 }
